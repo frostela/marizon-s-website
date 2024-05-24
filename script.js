3107fb6836b1c9e35cca62fa6adbc8619177aa7c
Frostela <mainakdashlr@gmail.com>
2024-05-24 16:21:06 +0530

    vercel.js added

--- script.js
+++ script.js
@@ -70,5 +70,15 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.removeChild("loader");
       })
     });
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
     
   });
